b7f8bdd0992dcadfbcc56cb16232c2c3ea839650
Joao-P Medeiros <Joao-P.Medeiros@btgpactual.com>
2022-08-02 14:42:27 -0300

    feat: get memory allocation by garbage collection gen

--- src/Demo/Worker.cs
+++ src/Demo/Worker.cs
@@ -22,15 +22,13 @@ public class Worker : BackgroundService
             string? filePath = Environment.GetEnvironmentVariable("INPUT_FILE_PATH");
             string? currentLine = null;
             int totalLines = 0;
+            int[] startCollectionCount = new int[3] { GC.CollectionCount(0), GC.CollectionCount(1), GC.CollectionCount(2) };
 
             if (File.Exists(filePath))
             {
                 using (Stream stream = File.OpenRead(filePath))
                 using (StreamReader reader = new StreamReader(stream))
-                while ((currentLine = reader.ReadLine()) != null)
-                {
-                    totalLines++;                    
-                }
+                while ((currentLine = reader.ReadLine()) != null) totalLines++;
             } else
             {
                 _logger.LogWarning($"Worker has not found input file {filePath}. Searching again on next iteration");
@@ -39,8 +37,16 @@ public class Worker : BackgroundService
             DateTimeOffset endAt = DateTimeOffset.Now;
 
             _logger.LogInformation($"Worker stopping at: {endAt}");
+            
             _logger.LogInformation($"Time spent: {endAt.ToUnixTimeMilliseconds() - startAt.ToUnixTimeMilliseconds()} ms");
-            _logger.LogInformation($"Memory spent: {Process.GetCurrentProcess().WorkingSet64/1024/1024} mb");
+            
+            for (int i = 0; i < startCollectionCount.Length; i++)
+            {
+                _logger.LogInformation($"Gen {i} Allocations: {GC.CollectionCount(i) - startCollectionCount[i]}");
+            }
+
+
+            _logger.LogInformation($"Memory spent: {Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024} mb");
 
             await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
         }
